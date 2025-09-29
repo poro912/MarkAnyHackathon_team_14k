4b8b5702b800439e002dc3a2cc5f879fbe7319c8
poro912 <ec2-user@ip-172-31-22-252.ec2.internal>
2025-09-29 15:08:47 +0000

    modify start code, modify permission

--- start_server.sh
+++ start_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 서버 시작 스크립트
-cd /home/ec2-user/MarkAnyHackathon_team_14k/server
+# cd /home/ec2-user/MarkAnyHackathon_team_14k/server
 
 # 서버 시작
-sudo python3 local_backend.py
+sudo nohup python3 server/local_backend.py &
