f3586cfdd64768823dabfbc94ef9c631a45491f2
poro912 <ec2-user@ip-172-31-22-252.ec2.internal>
2025-09-29 14:54:10 +0000

    modify start scripts and perminsion

--- start_server.sh
+++ start_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 서버 시작 스크립트
-cd /path/to/MarkAnyHackathon_team_14k/server
+cd /home/ec2-user/MarkAnyHackathon_team_14k/server
 
 # 서버 시작
-python3 local_backend.py
+sudo python3 local_backend.py
